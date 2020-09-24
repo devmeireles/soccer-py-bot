b718ed6a2c4f597ee7e39ec837b710dccaceb0e4
Gabriel Meireles <gmeireles@2mundos.net>
2020-09-24 14:46:29 -0300

    Adding premier league scorers runner

---
+++ runners/premier-league-scorers.py
@@ -0,0 +1,96 @@
+players = [
+    {
+        'photo': 'https://i2-prod.leicestermercury.co.uk/sport/football/article3665184.ece/ALTERNATES/s1200c/0_Jamie-Vardy-135.jpg',
+        'profile': 'https://www.transfermarkt.com/jamie-vardy/leistungsdaten/spieler/197838/plus/0?saison=2019',
+        'text': '2019/2020 Season',
+        'filter': 'Premier League',
+        'club': 'https://tmssl.akamaized.net/images/wappen/head/1003.png?lm=1472229265'
+    },
+    {
+        'photo': 'https://i2-prod.birminghammail.co.uk/incoming/article18026262.ece/ALTERNATES/s1200c/0_Pierre-Emerick-Aubameyang.jpg',
+        'profile': 'https://www.transfermarkt.com/pierre-emerick-aubameyang/leistungsdaten/spieler/58864/plus/0?saison=2018',
+        'text': '2018/2019 Season',
+        'filter': 'Premier League',
+        'club': 'https://tmssl.akamaized.net/images/wappen/head/11.png?lm=1489787850'
+    },
+    {
+        'photo': 'https://i.pinimg.com/originals/e6/34/27/e63427dc38816f1559f71aa67e1ac800.jpg',
+        'profile': 'https://www.transfermarkt.com/sadio-mane/leistungsdaten/spieler/200512/plus/0?saison=2018',
+        'text': '2018/2019 Season',
+        'filter': 'Premier League',
+        'club': 'https://tmssl.akamaized.net/images/wappen/head/31.png?lm=1456567819'
+    },
+    {
+        'photo': 'https://i.guim.co.uk/img/media/ddab8e3e1ba5a0afe7bf30596ce0f0a505802e8e/304_0_2857_1714/master/2857.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=968e3f614b1180c77212736b1e0664b6',
+        'profile': 'https://www.transfermarkt.com/mohamed-salah/leistungsdaten/spieler/148455/plus/0?saison=2018',
+        'text': '2018/2019 Season',
+        'filter': 'Premier League',
+        'club': 'https://tmssl.akamaized.net/images/wappen/head/31.png?lm=1456567819'
+    },
+    {
+        'photo': 'https://amayei.nyc3.digitaloceanspaces.com/2018/09/salah-18.jpg',
+        'profile': 'https://www.transfermarkt.com/mohamed-salah/leistungsdaten/spieler/148455/plus/0?saison=2017',
+        'text': '2017/2018 Season',
+        'filter': 'Premier League',
+        'club': 'https://tmssl.akamaized.net/images/wappen/head/31.png?lm=1456567819'
+    },
+    {
+        'photo': 'https://media.bleacherreport.com/f_auto,w_800,h_800,q_auto,c_fill/br-img-images/003/234/438/b1b8f1b7ce322d987526a8f6e19b07e3_crop_north.jpg',
+        'profile': 'https://www.transfermarkt.com/harry-kane/leistungsdaten/spieler/132098/plus/0?saison=2016',
+        'text': '2016/2017 Season',
+        'filter': 'Premier League',
+        'club': 'https://tmssl.akamaized.net/images/wappen/head/148.png?lm=1544345801'
+    },
+    {
+        'photo': 'https://img.bleacherreport.net/img/images/photos/003/585/229/hi-res-8ac7e8a1673c9e6f27813a2e7f299720_crop_exact.jpg?w=1200&h=1200&q=75',
+        'profile': 'https://www.transfermarkt.com/harry-kane/leistungsdaten/spieler/132098/plus/0?saison=2015',
+        'text': '2015/2016 Season',
+        'filter': 'Premier League',
+        'club': 'https://tmssl.akamaized.net/images/wappen/head/148.png?lm=1544345801'
+    },
+    {
+        'photo': 'https://i2-prod.manchestereveningnews.co.uk/incoming/article9321824.ece/ALTERNATES/s1200c/1_aguero.jpg',
+        'profile': 'https://www.transfermarkt.com/sergio-aguero/leistungsdaten/spieler/26399/plus/0?saison=2014',
+        'text': '2014/2015 Season',
+        'filter': 'Premier League',
+        'club': 'https://www.logolynx.com/images/logolynx/s_02/0273fa5940eab11efd0e3f3eb3f1fe13.png'
+    },
+    {
+        'photo': 'https://img.bleacherreport.net/img/images/photos/002/696/777/hi-res-459715843-luis-suarez-of-liverpool-looks-on-during-the-barclays_crop_exact.jpg?w=1200&h=1200&q=50',
+        'profile': 'https://www.transfermarkt.com/luis-suarez/leistungsdaten/spieler/44352/plus/0?saison=2013',
+        'text': '2013/2014 Season',
+        'filter': 'Premier League',
+        'club': 'https://tmssl.akamaized.net/images/wappen/head/31.png?lm=1456567819'
+    },
+    {
+        'photo': 'https://pbs.twimg.com/media/BR5B6eECQAA9vMD.jpg:large',
+        'profile': 'https://www.transfermarkt.com/robin-van-persie/leistungsdaten/spieler/4380/plus/0?saison=2012',
+        'text': '2012/2013 Season',
+        'filter': 'Premier League',
+        'club': 'https://tmssl.akamaized.net/images/wappen/head/985.png?lm=1457975903'
+    },
+    {
+        'photo': 'https://img.bleacherreport.net/img/images/photos/001/698/146/140787468_crop_exact.jpg?w=1200&h=1200&q=75',
+        'profile': 'https://www.transfermarkt.com/robin-van-persie/leistungsdaten/spieler/4380/plus/0?saison=2011',
+        'text': '2011/2012 Season',
+        'filter': 'Premier League',
+        'club': 'https://tmssl.akamaized.net/images/wappen/head/11.png?lm=1489787850'
+    },
+    {
+        'photo': 'https://cdn.vox-cdn.com/thumbor/_WzT3lBa6D8TGArzOvKEYq6J9lw=/0x0:599x399/1400x1400/filters:focal(0x0:599x399):format(jpeg)/cdn.vox-cdn.com/photo_images/1034103/GYI0061260922.jpg',
+        'profile': 'https://www.transfermarkt.com/dimitar-berbatov/leistungsdaten/spieler/65/plus/0?saison=2010',
+        'text': '2010/2011 Season',
+        'filter': 'Premier League',
+        'club': 'https://tmssl.akamaized.net/images/wappen/head/985.png?lm=1457975903'
+    },
+    {
+        'photo': 'https://media.bleacherreport.com/f_auto,w_800,h_800,q_auto,c_fill/br-img-images/003/639/593/hi-res-fd8369d335bdceb402b4ecf7c08d778c_crop_north.jpg',
+        'profile': 'https://www.transfermarkt.com/carlos-tevez/leistungsdaten/spieler/4276/plus/0?saison=2010',
+        'text': '2010/2011 Season',
+        'filter': 'Premier League',
+        'club': 'https://www.logolynx.com/images/logolynx/s_02/0273fa5940eab11efd0e3f3eb3f1fe13.png'
+    }
+]
+
+for player in players:
+    Images.save(player['photo'], player['profile'], player['text'], player['filter'], player['club'], 'lasts-pl-scorers')
